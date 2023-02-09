4a0fe3bbcba519ada5042f32ed269d0f9082c3c6
zaiky09 <zamilmozamil@outlook.com>
2023-02-09 22:06:24 +0300

    test passed

--- enumerables.rb
+++ enumerables.rb
@@ -12,13 +12,13 @@ end
 # given an array of spicy foods, **return an array of strings**
 # with the names of each spicy food
 def get_names(spicy_foods)
-  # your code here
+    spicy_str = spicy_foods.map { |chakula| "#{chakula[:name]}"} 
 end
 
 # given an array of spicy foods, **return an array of hashes** 
 # where the heat level of the food is greater than 5
 def spiciest_foods(spicy_foods)
-  # your code here
+    spicy_heat_level = spicy_foods.filter { |chakula| chakula[:heat_level] > 5}
 end
 
 # given an array of spicy foods, **output to the terminal**
@@ -27,19 +27,19 @@ end
 # HINT: you can use * with a string to produce the correct number of 🌶 emoji. 
 # "hello" * 3 == "hellohellohello"
 def print_spicy_foods(spicy_foods)
-  # your code here
+    spicy_foods.each { |chakula| puts "#{chakula[:name]} (#{chakula[:cuisine]}) | Heat Level: #{'🌶' * chakula[:heat_level]}"}
 end
 
 # given an array of spicy foods and a string representing a cuisine, **return a single hash**  
 # for the spicy food whose cuisine matches the cuisine being passed to the method
 def get_spicy_food_by_cuisine(spicy_foods, cuisine)
-  # your code here
+    spicy_foods.find {|chakula| chakula[:cuisine] == cuisine}
 end
 
 # Given an array of spicy foods, **return an array of hashes** 
 # sorted by heat level from lowest to highest
 def sort_by_heat(spicy_foods)
-  # your code here
+    sorted_foods = spicy_foods.sort { |chakula1, chakula2| chakula1[:heat_level] <=> chakula2[:heat_level]}
 end
 
 # given an array of spicy foods, output to the terminal ONLY 
@@ -47,11 +47,13 @@ end
 # Buffalo Wings (American) | Heat Level: 🌶🌶🌶
 # HINT: Try to use methods you've already written to solve this!
 def print_spiciest_foods(spicy_foods)
-  # your code here
+  spicy_foods.filter { |chakula| chakula[:heat_level] > 5 }.each { |chakula|
+  puts "#{chakula[:name]} (#{chakula[:cuisine]}) | Heat Level: #{'🌶' * chakula[:heat_level]}"}
 end
 
 # given an array of spicy foods, return an integer representing 
 # the average heat level of all the spicy foods in the array
 def average_heat_level(spicy_foods)
-  # your code here
+  heat_levels = spicy_foods.map { |chakula| chakula[:heat_level] }
+  heat_levels.sum / heat_levels.length
 end
